1e1e61fed1e99fdc0d040d5dbee3a971b9ad01d9
shuvo <minutestech745@gmail.com>
2024-08-01 18:29:18 +0600

    Fixing and organizing my code.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { footerOptions } from '../utils/LinkOptions'
 
 function Footer() {
     return (
-        <div className='w-full mt-28 pb-6 flex flex-col items-center justify-center gap-5'>
+        <div className='w-full mt-28 flex flex-col items-center justify-center gap-5'>
             <div className='flex justify-center max-md:px-5 items-center flex-wrap gap-4'>
                 {
                     footerOptions.map((item, index) =>
